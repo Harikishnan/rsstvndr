e7205df64c7af03b19012745cb293bec98f2b5ef
Hari Krishnan <75353186+Harikishnan@users.noreply.github.com>
2021-01-19 19:41:32 -1200

    Update notification.js

--- notification.js
+++ notification.js
@@ -1,19 +1,19 @@
-var modal=document.getElementById("myModal"),
-body=document.getElementsByTagName("body"),
-container=document.getElementById("myContainer"),
-btnOpen=document.getElementById("myBtn"),
-btnClose=document.getElementById("closeModal");
-var modal2=document.getElementById("myModal2"),
-btnClose1=document.getElementById("closeModal2");
+// var modal=document.getElementById("myModal"),
+// body=document.getElementsByTagName("body"),
+// container=document.getElementById("myContainer"),
+// btnOpen=document.getElementById("myBtn"),
+// btnClose=document.getElementById("closeModal");
+// var modal2=document.getElementById("myModal2"),
+// btnClose1=document.getElementById("closeModal2");
 
-var myvar;
-var myvar2;
-function myFunction(){
-    myvar=setTimeout(shownotify, 7000);
-    //myvar2=setTimeout(shownotify2, 5000000);//
-}
+// var myvar;
+// var myvar2;
+// function myFunction(){
+//     myvar=setTimeout(shownotify, 7000);
+//     //myvar2=setTimeout(shownotify2, 5000000);//
+// }
 
 
-function shownotify (){
-    window.location.href="bloodregisteredmembers.html"
-}
+// function shownotify (){
+//     window.location.href="bloodregisteredmembers.html"
+// }
