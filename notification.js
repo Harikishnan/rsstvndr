f520308b734e5478b204ed396ca93c59a695e3bf
Hari Krishnan <75353186+Harikishnan@users.noreply.github.com>
2021-01-19 19:35:59 -1200

    Update notification.js

--- notification.js
+++ notification.js
@@ -9,37 +9,11 @@ btnClose1=document.getElementById("closeModal2");
 var myvar;
 var myvar2;
 function myFunction(){
-    myvar=setTimeout(shownotify, 10000);
-    myvar2=setTimeout(shownotify2, 30000);
+    myvar=setTimeout(shownotify, 7000);
+    //myvar2=setTimeout(shownotify2, 5000000);//
 }
 
 
 function shownotify (){
-    document.getElementById("myModal").className="Modal is-visuallyHidden",
-    setTimeout(function(){
-        document.getElementById("myContainer").className="MainContainer is-blurred"
-        document.getElementById("myModal").className="Modal"
-    },100),container.parentElement.className="ModalOpen"
+    window.location.href="bloodregisteredmembers.html"
 }
-
-function shownotify2 (){
-    document.getElementById("myModal2").className="Modal is-visuallyHidden",
-    setTimeout(function(){
-        document.getElementById("myContainer").className="MainContainer is-blurred"
-        document.getElementById("myModal2").className="Modal"
-    },300),container.parentElement.className="ModalOpen"
-}
-
-
-btnClose.onclick=function(){modal.className="Modal  is-visuallyHidden",
-body.className="",container.className="MainContainer",container.parentElement.
-className=""},window.onclick=function(e){e.target==modal&&(modal.className="Modal is-hidden",
-body.className="",container.className="MainContainer",
-container.parentElement.className="")};
-
-
-btnClose1.onclick=function(){modal.className="Modal  is-hidden",
-body.className="",container.className="MainContainer",container.parentElement.
-className="",className="",modal2.className="Modal  is-hidden",
-body.className="",container.className="MainContainer",container.parentElement.
-className=""}
