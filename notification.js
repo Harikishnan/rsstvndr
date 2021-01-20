9412453fd87880709f5eeee1e6dc250bf5507a7c
Hari Krishnan <75353186+Harikishnan@users.noreply.github.com>
2021-01-19 20:04:02 -1200

    Update notification.js

--- notification.js
+++ notification.js
@@ -6,14 +6,14 @@
 // var modal2=document.getElementById("myModal2"),
 // btnClose1=document.getElementById("closeModal2");
 
-// var myvar;
-// var myvar2;
-// function myFunction(){
-//     myvar=setTimeout(shownotify, 7000);
+ var myvar;
+ var myvar2;
+ function myFunction(){
+     myvar=setTimeout(shownotify, 7000);
 //     //myvar2=setTimeout(shownotify2, 5000000);//
-// }
+ }
 
 
-// function shownotify (){
-//     window.location.href="bloodregisteredmembers.html"
-// }
+ function shownotify (){
+     window.location.href="bloodregisteredmembers.html"
+ }
